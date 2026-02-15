adb1089d68bd367e4b69a933ec53279848d2009c
olcaytaner <olcay.yildiz@ozyegin.edu.tr>
2026-02-15 23:05:05 +0300

    Upgraded to nodeNext.

--- dist/PolarityType.js
+++ dist/PolarityType.js
@@ -1,20 +1,10 @@
-(function (factory) {
-    if (typeof module === "object" && typeof module.exports === "object") {
-        var v = factory(require, exports);
-        if (v !== undefined) module.exports = v;
-    }
-    else if (typeof define === "function" && define.amd) {
-        define(["require", "exports"], factory);
-    }
-})(function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    exports.PolarityType = void 0;
-    var PolarityType;
-    (function (PolarityType) {
-        PolarityType[PolarityType["POSITIVE"] = 0] = "POSITIVE";
-        PolarityType[PolarityType["NEGATIVE"] = 1] = "NEGATIVE";
-        PolarityType[PolarityType["NEUTRAL"] = 2] = "NEUTRAL";
-    })(PolarityType = exports.PolarityType || (exports.PolarityType = {}));
-});
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PolarityType = void 0;
+var PolarityType;
+(function (PolarityType) {
+    PolarityType[PolarityType["POSITIVE"] = 0] = "POSITIVE";
+    PolarityType[PolarityType["NEGATIVE"] = 1] = "NEGATIVE";
+    PolarityType[PolarityType["NEUTRAL"] = 2] = "NEUTRAL";
+})(PolarityType || (exports.PolarityType = PolarityType = {}));
 //# sourceMappingURL=PolarityType.js.map
--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,11 @@
+"use strict";
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
 }) : (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     o[k2] = m[k];
@@ -8,21 +13,10 @@ var __createBinding = (this && this.__createBinding) || (Object.create ? (functi
 var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
-(function (factory) {
-    if (typeof module === "object" && typeof module.exports === "object") {
-        var v = factory(require, exports);
-        if (v !== undefined) module.exports = v;
-    }
-    else if (typeof define === "function" && define.amd) {
-        define(["require", "exports", "./PolarityType", "./SentiNet", "./SentiSynSet", "./SentiLiteral", "./SentiLiteralNet"], factory);
-    }
-})(function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    __exportStar(require("./PolarityType"), exports);
-    __exportStar(require("./SentiNet"), exports);
-    __exportStar(require("./SentiSynSet"), exports);
-    __exportStar(require("./SentiLiteral"), exports);
-    __exportStar(require("./SentiLiteralNet"), exports);
-});
+Object.defineProperty(exports, "__esModule", { value: true });
+__exportStar(require("./PolarityType"), exports);
+__exportStar(require("./SentiNet"), exports);
+__exportStar(require("./SentiSynSet"), exports);
+__exportStar(require("./SentiLiteral"), exports);
+__exportStar(require("./SentiLiteralNet"), exports);
 //# sourceMappingURL=index.js.map
